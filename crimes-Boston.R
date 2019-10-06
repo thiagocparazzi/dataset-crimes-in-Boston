bac1d5c2bb78204548c929cefb37f5f0ad8448d4
thiagocparazzi <thiagocparazzi@gmail.com>
2019-10-06 15:50:12 -0300

    v2.0 - Completo

--- crimes-Boston.R
+++ crimes-Boston.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(ggplot2)
 library(dslabs)
 
-#abaixo inserir nos parenteses o caminho para wine.data
+#abaixo inserir nos parenteses o caminho para crimes data
 crimes = read.csv('D:/Dados/Downloads/crime.csv', header = TRUE)
 View(crimes)
 
@@ -46,21 +46,59 @@ ggplot(data = crime_homicide) +
   geom_point(aes(x = Lat, y = Long, col = DISTRICT)) +
   ggtitle("Homicide crime")
 
-#series temporais
-ggplot(crime_type, aes(YEAR, MONTH, col = OFFENSE_CODE_GROUP)) +
-  geom_line()
+#series temporais#
 
-nrow(subset(crime_larceny, YEAR==2015))
-nrow(subset(crime_larceny, YEAR==2016))     
-nrow(subset(crime_larceny, YEAR==2017))
-nrow(subset(crime_larceny, YEAR==2018))
+#craindo um dataset com os numeros de cada ano para cada tipo de crime
+larceny_evolution <-c (nrow(subset(crime_larceny, YEAR==2015)), nrow(subset(crime_larceny, YEAR==2016)), nrow(subset(crime_larceny, YEAR==2017)), nrow(subset(crime_larceny, YEAR==2018)))
+drug_evolution <-c (nrow(subset(crime_drug_violation, YEAR==2015)), nrow(subset(crime_drug_violation, YEAR==2016)), nrow(subset(crime_drug_violation, YEAR==2017)), nrow(subset(crime_drug_violation, YEAR==2018)))
+homicide_evolution <-c (nrow(subset(crime_homicide, YEAR==2015)), nrow(subset(crime_homicide, YEAR==2016)), nrow(subset(crime_homicide, YEAR==2017)), nrow(subset(crime_homicide, YEAR==2018)))
+years <-c (2015, 2016, 2017, 2018)
 
-nrow(subset(crime_drug_violation, YEAR==2015))
-nrow(subset(crime_drug_violation, YEAR==2016))     
-nrow(subset(crime_drug_violation, YEAR==2017))
-nrow(subset(crime_drug_violation, YEAR==2018))
+crime_evolution <- data.frame(larceny_evolution, drug_evolution, homicide_evolution, years)
 
-nrow(subset(crime_homicide, YEAR==2015))
-nrow(subset(crime_homicide, YEAR==2016))     
-nrow(subset(crime_homicide, YEAR==2017))
-nrow(subset(crime_homicide, YEAR==2018))
+#serie temporal em linha
+ggplot(crime_evolution, aes(years, larceny_evolution)) +
+  geom_line()+
+  geom_point()+
+  ggtitle("Larceny crime evolution through the years")
+
+ggplot(crime_evolution, aes(years, drug_evolution, col = years)) +
+  geom_line()+
+  geom_point()+
+  ggtitle("Drug Violation crime evolution through the years")
+
+ggplot(crime_evolution, aes(years, homicide_evolution, col = years)) +
+  geom_line()+
+  geom_point()+
+  ggtitle("Homicide crime evolution through the years")
+
+#procurando os distritos mais violentos em geral
+ggplot(data=crime_type, aes(x=DISTRICT, y=OFFENSE_CODE_GROUP, fill = OFFENSE_CODE_GROUP)) +
+  geom_bar(stat="identity", width=0.5)
+
+#filtra-los para os tipos de crimes e distritos mais violentos
+D4 <- filter(crime_type, DISTRICT %in% ("D4"))
+A1 <- filter(crime_type, DISTRICT %in% ("A1"))
+B2 <- filter(crime_type, DISTRICT %in% ("B2"))
+
+d4_evolution <-c (nrow(subset(D4, YEAR == 2015)), nrow(subset(D4, YEAR == 2016)), nrow(subset(D4, YEAR == 2017)), nrow(subset(D4, YEAR == 2018)))
+a1_evolution <-c (nrow(subset(A1, YEAR == 2015)), nrow(subset(A1, YEAR == 2016)), nrow(subset(A1, YEAR == 2017)), nrow(subset(A1, YEAR == 2018)))
+b2_evolution <-c (nrow(subset(B2, YEAR == 2015)), nrow(subset(B2, YEAR == 2016)), nrow(subset(B2, YEAR == 2017)), nrow(subset(B2, YEAR == 2018)))
+
+districts_evolution <- data.frame(d4_evolution, a1_evolution, b2_evolution, years)
+View(districts_evolution)
+
+ggplot(districts_evolution, aes(years, d4_evolution)) +
+  geom_line()+
+  geom_point()+
+  ggtitle("District D4 evolution through the years")
+
+ggplot(districts_evolution, aes(years, a1_evolution)) +
+  geom_line()+
+  geom_point()+
+  ggtitle("District A1 evolution through the years")
+
+ggplot(districts_evolution, aes(years, b2_evolution)) +
+  geom_line()+
+  geom_point()+
+  ggtitle("District B2 evolution through the years")
